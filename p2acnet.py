f2be69fd19d1c3831a30db3c3a4964a23a031edb
Alex Sippel <asippel89@gmail.com>
2012-10-13 13:16:11 -0500

    Worked on script via dorothy

--- p2acnet.py
+++ p2acnet.py
@@ -7,9 +7,15 @@ import numpy as np
 
 r = requests.get('http://www-ad.fnal.gov/cgi-bin/acl.pl?acl=logger_get/double/node=fastest/start=11-OCT-2012-12:30/end=11-OCT-2012-12:35+E:HADC02')
 raw = r.content
+print len(raw)
+
 
 data_list = []
 for line in raw:
-    dataline = line.strip().split('   ')
+    dataline = line.split('  ')
     data_list.append(dataline)
 data = np.array(data_list)
+
+print 'Data = ', data
+
+# Do you see this message?

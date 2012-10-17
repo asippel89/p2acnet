03ad392b2b14ce7193e3efc3d78306cc591c28a9
Alex Sippel <asippel89@gmail.com>
2012-10-17 15:35:50 -0500

    p2acnet now queries list of channels, returns dictionary; p2acnet_test now more advanced with ability to make multiple queries entered in an array

--- p2acnet.py
+++ p2acnet.py
@@ -5,6 +5,20 @@ import datetime as dt
 import matplotlib.pyplot as plt
 import matplotlib.dates as mdates
 
+class P2ACNETGroup(object):
+
+    def __init__(self, channel_list, start_time, end_time):
+        self.channel_list = channel_list
+        self.start_time = start_time
+        self.end_time = end_time
+            
+    def run_group(self):
+        data_dict = {}
+        for channel in self.channel_list:
+            new_instance = P2ACNET(channel, self.start_time, self.end_time)
+            data_dict[channel] = new_instance.parse_query()
+        return data_dict
+
 class P2ACNET(object):
     ''' This class responds to the full http query content and does splitlines() on it,
         then goes through with the parsing.'''
@@ -26,29 +40,34 @@ class P2ACNET(object):
         data_list = []
         for element in self.raw:
             data_split = element.split('   ')
-            datetime_el = mdates.date2num(dt.datetime.strptime(data_split[0], '%d-%b-%Y %H:%M:%S.%f'))
+            datetime_el = dt.datetime.strptime(data_split[0], '%d-%b-%Y %H:%M:%S.%f')
             value_el = float(data_split[1].strip())
             data_list.append([datetime_el, value_el])
         self.data_array = np.array(data_list)
+        self.delta_t = self.data_array[1][0] - self.data_array[0][0]
         print "\tNumber of returned time-value pairs:", self.data_array.shape[0]
         return self.data_array
 
 def plot_response(data_array):
+    delta_t = mdates.relativedelta(data_array[1][0], data_array[0][0])
+    # print "Time scale =", dt
     plt.close('all')
     fig, ax = plt.subplots(1)
-    ax.plot(data_array[0], data_array[1])
+    ax.plot_date(data_array[0], data_array[1])
     fig.autofmt_xdate()
-    hfmt = mdates.DateFormatter('%m/%d %H:%M')
-    ax.xaxis.set_major_locator(mdates.DayLocator())
+    hfmt = mdates.AutoDateFormatter()
+    ax.xaxis.set_major_locator(mdates.AutoDateLocator())
     ax.xaxis.set_major_formatter(hfmt)
     # set the format of dates in the toolbar
     ax.fmt_xdata = mdates.DateFormatter('%y-%m-%dT%H:%M:%S')
     plt.show()
     
 if __name__ == '__main__':
-
-    query1 = P2ACNET('E:TCIP', '13-OCT-2012-12:30', '14-OCT-2012-12:35')
-    result = query1.parse_query()
-    print "data_array[0][0] = ", result[0][0]
-    print "data_array[0][1] = ", result[0][1]
-    print "data_array shape = ", result.shape
+    
+    channel_list = ['E:TCIP', 'E:TNIP1', 'E:TNIP0']
+    query1 = P2ACNETGroup(channel_list, '17-OCT-2012-12:30', '17-OCT-2012-14:30')
+    result = query1.run_group()
+    # print "data_array[0][0] = ", result[0][0]
+    # print "data_array[0][1] = ", result[0][1]
+    # print "data_array shape = ", result.shape
+    
--- p2acnet_test.py
+++ p2acnet_test.py
@@ -1,24 +1,46 @@
 
+import numpy as np
 from datetime import datetime
 
 class P2ACNETTester(object):
     
-    def __init__(self, p2acnet_version_list, query_list):
-        for element in p2acnet_version_list:
-            self.test(element, query_list)
+    def __init__(self, p2acnet_version_list, query_array):
+        self.p2acnet_version_list = p2acnet_version_list
+        if len(query_array.shape) < 2:
+            self.query_array = np.array([query_array])
+        else:
+            self.query_array = query_array
 
-    def test(self, p2acnet_version, query_list):
-        print "Begin test with", p2acnet_version, "class..."
+    def runtests(self):
+        for element in self.p2acnet_version_list:
+            for x in self.query_array:
+                test_instance = _P2ACNETTest(element, x)
+                test_instance.test()
+        return
+
+class _P2ACNETTest(object):
+
+    def __init__(self, p2acnet_version, query):
+        self.p2acnet_version = p2acnet_version
+        self.query = query
+        
+    def test(self):
+        print "Begin test with", self.p2acnet_version, "class..."
         tstart = datetime.now()
-        query = p2acnet_version.P2ACNET(query_list[0], query_list[1], query_list[2])  # Could use a list comprehension here?
+        query = self.p2acnet_version.P2ACNET(query_array[0], query_array[1], query_array[2])  # Could use a list comprehension here?
         result = query.parse_query()
         tend = datetime.now()
         dt = tend - tstart
         print "Time Spent:", dt
         return
 
+    def __str__(self):
+        '''Return a more readable representation'''
+        return "%a Test" % (self.p2acnet_version)
+
 if __name__ == '__main__':
     import p2acnet, p2acnet2
     p2acnet_version_list = [p2acnet, p2acnet2]
-    query_list = ['E:HTC06', '10-JUN-2012-12:30', '14-OCT-2012-12:30']
-    result = P2ACNETTester(p2acnet_version_list, query_list)
+    query_array = np.array(['E:HTC06', '10-SEP-2012-12:30', '14-OCT-2012-12:30'])
+    result = P2ACNETTester(p2acnet_version_list, query_array)
+    result.runtests()

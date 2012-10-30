8d0d5deba3c7f650ae676f3f48b74cf429c9797a
Alex Sippel <asippel89@gmail.com>
2012-10-30 09:22:05 -0500

    local changes, may be out dated

--- p2acnet.py
+++ p2acnet.py
@@ -6,9 +6,9 @@ import matplotlib.pyplot as plt
 import matplotlib.dates as mdates
 from collections import defaultdict
 
-class P2ACNETGroup(object):
+class P2ACNET(object):
     '''
-    This class acts like a factory, creating instances of the P2ACNET class for each channel in channel_list and
+    This class acts like a factory, creating instances of the P2ACNETSingle class for each channel in channel_list and
     recording/displaying the results.
     '''
 
@@ -19,18 +19,18 @@ class P2ACNETGroup(object):
         
     def run_group(self):
         '''
-        This method creates instances of the P2ACNET class for each channel in the list and places them in a dictionary.
-        One can access the P2ACNET class methods for each channel by looping over the dictionary.
+        This method creates instances of the P2ACNETSingle class for each channel in the list and places them in a dictionary.
+        One can access the P2ACNETSingle class methods for each channel by looping over the dictionary.
         '''
         instance_dict = {}
         for channel in self.channel_list:
-            new_instance = P2ACNET(channel, self.start_time, self.end_time)
+            new_instance = P2ACNETSingle(channel, self.start_time, self.end_time)
             instance_dict[channel] = new_instance
         return instance_dict
 
     def plot_group(self, title="", ylabel=""):
         '''
-        This method iterates over the instance_dict, calling the plot_single method of the P2ACNET class for each instance
+        This method iterates over the instance_dict, calling the plot_single method of the P2ACNETSingle class for each instance
         of that class in the dictionary (basically each channel). The resulting plot has automatically scaled dates as well
         as a legend which shows the channels. A title and y-label can be supplied, though in future versions I hope to include
         automatic y-axis labeling based on queried channel units (along with subplots for channels of different units).
@@ -69,7 +69,7 @@ class P2ACNETGroup(object):
             data_dict[channel] = instance_dict[channel].get_data()
         return data_dict
             
-class P2ACNET(object):
+class P2ACNETSingle(object):
     '''
     This class describes the content of a single channel query to Fermilab's ACNET. It accesses a time series
     from one of ACNET's data loggers by making an HTTP get request. The contents of the HTTP request is a short
@@ -145,7 +145,7 @@ class P2ACNET(object):
     def plot_single(self, ax=None):
         '''
         This method plots the data for a data_array with automatic matplotlib formatting for the dates
-        on the x-axis. The ax option is used in the P2ACNETGroup class to combine the plots for several
+        on the x-axis. The ax option is used in the P2ACNET class to combine the plots for several
         channels.
         '''
         if ax is None:            
@@ -159,13 +159,13 @@ class P2ACNET(object):
         return
     
 if __name__ == '__main__':
-    #-------------Test multiple ;channels using P2ACNETGroup-----------#
+    #-------------Test multiple ;channels using P2ACNET-----------#
     TIFO_list = ['E:TCIP', 'E:TNIP0', 'E:TNIP1', 'E:TNESIP', 'E:TEIP0', 'E:TEIP1', 'E:TEESIP']
     LIFO_list = ['E:LCIP', 'E:LNIP0', 'E:LNIP1', 'E:LNESIP', 'E:LEIP0', 'E:LEIP1', 'E:LEESIP']
     Temp_env = ['G:OUTTMP', 'G:WCHILL', 'G:HEATIX', 'G:DEWPNT']
     start_time = '24-OCT-2012-17:30'
     end_time = 'Now'
-    query = P2ACNETGroup(TIFO_list, start_time, end_time)
+    query = P2ACNET(TIFO_list, start_time, end_time)
     plot = query.plot_group()
     # data = query.get_group_data()
     # print data
